c077aa7693da81eea52a9cf8ac7733f120bc8940
bbishay <50244538+bbishay@users.noreply.github.com>
2019-10-31 11:46:52 -0400

    -

--- main.js
+++ main.js
@@ -71,5 +71,8 @@
 // ]);
 
 setTimeout(() =>{
-  document.querySelector('.music').innerHTML = '<div class="song">Chocolate Rain</div><div class="artist">Tayzon Day</div>'
+  document.querySelector('.music').innerHTML = '<div class="song">Chocolate Rain</div>'
+  setTimeout(() =>{
+    document.querySelectorAll('.music')[1].insertAdjacentHTML('beforeend', '<div class="artist">Tayzon Day</div>')
+  }, 5000)
 }, 1000)
